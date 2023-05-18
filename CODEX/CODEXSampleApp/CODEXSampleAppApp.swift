1170871384909c7bf9680f266b0cbbbb18c4e51c
Hosea H C Li <hosea.h.c.li@gmail.com>
2023-05-18 21:40:35 +0800

    Add sample app

---
+++ CODEX/CODEXSampleApp/CODEXSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CODEXSampleAppApp.swift
+//  CODEXSampleApp
+//
+//  Created by Hosea H C Li on 2023/5/18.
+//
+
+import SwiftUI
+
+@main
+struct CODEXSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
